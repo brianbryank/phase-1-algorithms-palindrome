5d115b27efdd8a6e46edb18331cd2be7c6e27824
brianbryank <brianbryan0125@gmail.com>
2023-07-29 18:18:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ function isPalindrome(word) {
   // Write your algorithm here
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+// You can run `nodeindex.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
